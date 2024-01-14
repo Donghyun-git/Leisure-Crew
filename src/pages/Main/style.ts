914438e7dfa092d3bbec5049c117187531008f96
donghyun-git <donghyun1113@hanmail.net>
2024-01-14 17:52:14 +0900

    design: 슬라이드 스타일

---
+++ src/pages/Main/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SlideWrapper = styled.div`
+  width: 100%;
+  height: 400px;
+  background-color: #dbdbdb;
+  margin-top: 80px;
+`;
+export const Wrapper = styled.div``;
